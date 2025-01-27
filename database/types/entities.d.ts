e31a82fd6564f03090399584585541016d05bed9
Reka <reka.reka.rn@gmail.com>
2025-01-28 03:21:48 +0700

    update BE

--- database/types/entities.d.ts
+++ database/types/entities.d.ts
@@ -1,14 +1,16 @@
 import type { InferInsertModel, InferModelFromColumns, InferSelectModel } from 'drizzle-orm';
 import * as schema from '../schema';
 
-export type Folder = InferSelectModel<typeof schema.folderSchema>;
+export type FolderT = InferSelectModel<typeof schema.folderSchema>;
 export type FolderInsertT = InferInsertModel<typeof schema.folderSchema>;
 export type FolderUpdateT = Partial<FolderInsertT>;
 export type FolderDeleteT = { id: string[] };
 
-export type File = InferSelectModel<typeof schema.fileSchema>;
+export type FileT = InferSelectModel<typeof schema.fileSchema>;
 export type FileInsertT = InferInsertModel<typeof schema.fileSchema>;
 export type FileCreateT = {name: string, text?: string, folderId?: string};
+export type FileUploadT = {files: File[], folderId?: string};
+export type FileModifyT = {text: string};
 export type FileUpdateT = Partial<FileInsertT>;
 export type FileDeleteT = { id: string[] };
 
